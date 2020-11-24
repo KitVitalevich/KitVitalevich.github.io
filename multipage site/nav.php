f082369b0b02396a3b64dc107730e70c7482eecd
KitVitalevich <72542634+KitVitalevich@users.noreply.github.com>
2020-11-24 16:39:36 +0700

    многостраничный сайт

---
+++ multipage site/nav.php
@@ -0,0 +1,25 @@
+<!-- Top menu -->
+<nav class="navbar navbar-fixed-top" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-navbar-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="index.html">Lancar - Bootstrap One-Page Portfolio Template</a>
+		</div>
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="top-navbar-1">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="index.html">Top</a></li>
+				<li><a href="services.html">Services</a></li>
+				<li><a href="portfolio.html">Portfolio</a></li>
+				<li><a href="prices.html">Prices</a></li>
+				<li><a href="about.html">About</a></li>
+				<li><a href="contacts.html">Contact</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
