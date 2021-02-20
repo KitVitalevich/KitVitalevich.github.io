62ac083b0989e307da74fe3fce123f68d79e8ec4
KitVitalevich <72542634+KitVitalevich@users.noreply.github.com>
2021-02-20 18:29:29 +0700

    blinds
    Жалюзи

--- blinds/js/script.js
+++ blinds/js/script.js
@@ -1,16 +1,20 @@
 
 $(document).ready(function () {
+  //Меню (кнопки бургер)
   $('.menu-nav__burger').click(function () {
     $('.menu-mobail').toggleClass('menu-mobail_active');
   });
   $('.menu-mobail__close').click(function () {
     $('.menu-mobail').toggleClass('menu-mobail_active');
   });
-
+  $('.menu-nav__item').click(function () {
+    $('.menu-mobail').toggleClass('menu-mobail_active');
+  });
+  //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+  //Слайдер в блоке с работами 
   $('.slider').slick({
     adaptiveHeight: true,
     slidesToScroll: 1,
-
     asNavFor: ".sliderbig",
   });
 
@@ -24,6 +28,7 @@ $(document).ready(function () {
   });
 
   //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+  //Слайдер в блоке с отзывами
   $('.review-slider').slick({
     adaptiveHeight: true,
     slidesToScroll: 1,
@@ -38,5 +43,69 @@ $(document).ready(function () {
     adaptiveHeight: true,
     slidesToScroll: 1,
   });
+  //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+  //Якорная анимация
+  $('a[href^="#header"]').on('click', function (event) {
+    // отменяем стандартное действие
+    event.preventDefault();
+    var sc = $(this).attr("href"),
+      dn = $(sc).offset().top;
+    /*
+    * sc - в переменную заносим информацию о том, к какому блоку надо перейти
+    * dn - определяем положение блока на странице
+    */
+    $('html, body').animate({ scrollTop: dn }, 1000);
+    /*
+    * 1000 скорость перехода в миллисекундах
+    */
+  });
+
+  $('a[href^="#goods"]').on('click', function (event) {
+    event.preventDefault();
+    var sc = $(this).attr("href"),
+      dn = $(sc).offset().top;
+    $('html, body').animate({ scrollTop: dn }, 1000);
+  });
+
+  $('a[href^="#slick"]').on('click', function (event) {
+    event.preventDefault();
+    var sc = $(this).attr("href"),
+      dn = $(sc).offset().top;
+    $('html, body').animate({ scrollTop: dn }, 1000);
+  });
+
+  $('a[href^="#sale"]').on('click', function (event) {
+    event.preventDefault();
+    var sc = $(this).attr("href"),
+      dn = $(sc).offset().top;
+    $('html, body').animate({ scrollTop: dn }, 1000);
+  });
+
+  $('a[href^="#review"]').on('click', function (event) {
+    event.preventDefault();
+    var sc = $(this).attr("href"),
+      dn = $(sc).offset().top;
+    $('html, body').animate({ scrollTop: dn }, 1000);
+  });
+
+  $('a[href^="#footer"]').on('click', function (event) {
+    event.preventDefault();
+    var sc = $(this).attr("href"),
+      dn = $(sc).offset().top;
+    $('html, body').animate({ scrollTop: dn }, 1000);
+  });
+  //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+  //Кнопка вверх
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.scrollup').fadeIn();
+    } else {
+      $('.scrollup').fadeOut();
+    }
+  });
+
+  $('.scrollup').click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
 });
-//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
