a42badbc526992e72b0c46916f830235e12639b7
KitVitalevich <72542634+KitVitalevich@users.noreply.github.com>
2021-04-06 20:08:49 +0700

    YokaService

--- YokaService/js/script.js
+++ YokaService/js/script.js
@@ -4,3 +4,16 @@ $(document).ready(function () {
     $('body').toggleClass('lock');
   });
 });
+
+//Слайдер gallery
+new Swiper('.gallery__container', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+  autoHeight: true,
+});
